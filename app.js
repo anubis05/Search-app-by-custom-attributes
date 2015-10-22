305bc286c981f41e26621b05cf6aca6bc8a9f8ab
Sarthak Gangopadhyay <sarthak@apigee.com>
2015-10-21 21:37:31 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,84 @@
+var express = require('express');
+var usergrid = require('usergrid');
+var config = require('./config');
+var Client = require('node-rest-client').Client;
+
+
+// Set up Express environment and enable it to read and write JavaScript
+var app = express();
+app.use(express.bodyParser());
+
+// Initialize Usergrid
+ usergridClient = new Client();
+var usergridClient = new usergrid.client({
+	'orgName' : config.organization,
+	'appName' : config.application,
+	'clientId' : config.clientId,
+	'clientSecret' : config.clientSecret,
+	'authType' : usergrid.AUTH_CLIENT_ID,
+	logging : config.logging
+});
+
+
+// The API starts here
+
+// GET /
+
+var rootTemplate = {
+	'employees' : {
+		'href' : './employees'
+	}
+};
+
+app.get('/migrate-edge-app',function(req,resp){
+	console.log("here");
+	client = new Client();
+	var args = {
+	  headers:{"Authorization": "Basic c2FydGhha0BhcGlnZWUuY29tOldlbGNvbWVfMTIz"}
+	};
+	var url="https://api.enterprise.apigee.com/v1/o/pixvy/apps/"+req.query.appId
+	client.get(url, args,function(data, response){
+		          console.log("inside client.get");
+							// parsed response body as js object
+							//console.log(data);
+							// raw response
+							console.log(data);
+              createProfile(data,req,resp);
+
+								resp.send("All you asked for is done :) ");
+							});
+					});
+
+// GET /profiles
+
+app.get('/profiles', function(req, res) {
+		getProfiles(req, res);
+});
+
+function createProfile(e, req, res) {
+	var opts = {
+		type : 'bboards',
+		name : e.id
+	};
+
+
+	usergridClient.createEntity(opts, function(err, o) {
+		if (err) {
+			res.jsonp(500, err);
+			return;
+		}
+		o.set(e);
+		o.save(function(err) {
+			if (err) {
+				res.jsonp(500, err);
+				return;
+			}
+			res.send(201);
+		});
+	});
+}
+
+// Listen for requests until the server is stopped
+
+app.listen(process.env.PORT || 9000);
+console.log('The server is running!');
---
+++ config.js
@@ -0,0 +1,10 @@
+/*
+ * Edit the defaults for your environment
+ */
+
+exports.organization = 'pixvy'
+exports.application = 'backend-app'
+exports.clientId = 'YXA6ldy0YHgPEeWD1n0bkOw9EA'
+exports.clientSecret = 'YXA6uJHAkn6oi3S8ajqqGEIRUMwmhL4'
+exports.tokenExpiration = 60000
+exports.logging = true
